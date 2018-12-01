48825c1f62457161e23b80fafd6068f79a2a3acf
Polukhin Vladimir <vovanmozg@gmail.com>
2018-12-02 00:49:49 +0300

    Initial

---
+++ src/screens/QuizItem.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+class QuizItem extends React.Component {
+  render() {
+    return (<View><Text>ku</Text></View>)
+  }
+}
+
+export default QuizItem;
