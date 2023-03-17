71f6fe72d428a525bca5df83067b84e6d98fdf90
vovanmozg <vovanmozg@gmail.com>
2023-03-17 08:22:35 +0400

    Add links processing

--- src/lib/formatHtml.ts
+++ src/lib/formatHtml.ts
@@ -33,8 +33,15 @@ function replaceGraveAccent(str: string): string {
   return str.replace(regex, '<span class="inlineCode"><code>$1</code></span>');
 }
 
+// https://stackoverflow.com/questions/37684/how-to-replace-plain-urls-with-links
+function insertLinks(str: string): string {
+  const replacePattern1 =
+    /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+  return str.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+}
+
 export function formatHtml(html: string): string {
-  const formattedHtml = replaceGraveAccent(replaceCode(html))
+  const formattedHtml = replaceGraveAccent(replaceCode(insertLinks(html)))
     .split('\n')
     .map((text: string) => `<p>${text}</p>`)
     .join('');
