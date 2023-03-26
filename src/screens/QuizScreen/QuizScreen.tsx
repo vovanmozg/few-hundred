6320082c6552bcee3f8c55b9f7cdfacbac29bf7c
vovanmozg <vovanmozg@gmail.com>
2023-03-26 22:20:11 +0400

    Extract useSetNavigationTitle

--- src/screens/QuizScreen/QuizScreen.tsx
+++ src/screens/QuizScreen/QuizScreen.tsx
@@ -1,28 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Box } from 'native-base';
 import { Quiz } from 'app/screens/QuizScreen/components/Quiz';
-import { useStore } from 'app/store/quizState';
+import { useSetNavigationTitle } from 'app/screens/QuizScreen/hooks/useSetNavigationTitle';
 import { RootStackParamList } from 'app/types/app';
-import type { TQuizState } from 'app/types/TQuizState';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export function QuizScreen({ navigation }: Props) {
-  const [quizItems, current] = useStore((state: TQuizState) => [
-    state.quizItems,
-    state.current,
-  ]);
-
-  useEffect(() => {
-    if (quizItems === null) {
-      return;
-    }
-
-    navigation.setOptions({
-      title: `Question ${current + 1} / ${Object.keys(quizItems).length}`,
-    });
-  }, [current, quizItems, navigation]);
+  useSetNavigationTitle({ navigation });
 
   return (
     <Box bg="white" p="1" h="100%" w="100%" alignSelf="center">
---
+++ src/screens/QuizScreen/hooks/useSetNavigationTitle.ts
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useStore } from 'app/store/quizState';
+import { RootStackParamList } from 'app/types/app';
+import { TQuizState } from 'app/types/TQuizState';
+
+type Props = Pick<
+  NativeStackScreenProps<RootStackParamList, 'Home'>,
+  'navigation'
+>;
+
+export function useSetNavigationTitle({ navigation }: Props) {
+  const [quizItems, current] = useStore((state: TQuizState) => [
+    state.quizItems,
+    state.current,
+  ]);
+
+  useEffect(() => {
+    if (quizItems === null) {
+      return;
+    }
+
+    navigation.setOptions({
+      title: `Question ${current + 1} / ${Object.keys(quizItems).length}`,
+    });
+  }, [current, quizItems, navigation]);
+}
