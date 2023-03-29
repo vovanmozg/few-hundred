1a93e5a012e3ea4ffb32e0cd77c76432ca04c491
vovanmozg <vovanmozg@gmail.com>
2023-03-30 00:19:04 +0400

    Add persistence for app store

--- src/store/appState.ts
+++ src/store/appState.ts
@@ -1,11 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 import { saveAnswerProgress } from 'app/store/appState/saveAnswerProgress';
 import { TAppState } from 'app/types/TAppState';
 
 export const useStore = create<TAppState>(
-  set =>
-    ({
-      answersProgress: {},
-      saveAnswerProgress: saveAnswerProgress(set),
-    } as TAppState),
+  persist(
+    set =>
+      ({
+        answersProgress: {},
+        saveAnswerProgress: saveAnswerProgress(set),
+      } as TAppState),
+    {
+      name: 'app',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
 );
