a76ba3148cd5b03eaaef14ae21d810f1da11bd9e
vovanmozg <vovanmozg@gmail.com>
2024-03-03 23:33:00 +0100

    Fix analytics

--- src/modules/app/AppWithAnalytics.tsx
+++ src/modules/app/AppWithAnalytics.tsx
@@ -6,8 +6,10 @@ import {
 import Config from 'react-native-config';
 import { AppWithNavigation } from 'app/modules/app/AppWithNavigation';
 
+// TODO: fix env vars from .env file
+//  https://github.com/lugg/react-native-config/issues/541#issuecomment-1491388207
 const segmentClient = createClient({
-  writeKey: Config.SEGMENT_KEY || '',
+  writeKey: 'ZhdCjVf2BnIuLOrXqL6t7dN3bi5x76mD', //Config.SEGMENT_KEY || '',
   trackAppLifecycleEvents: true,
 });
 
